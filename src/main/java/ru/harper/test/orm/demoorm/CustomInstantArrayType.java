efd6bc62cfda831876886ee291515f0bacdc2eb9
gg-harper <osstia@list.ru>
2022-11-18 09:53:04 +0300

    custom type start

---
+++ src/main/java/ru/harper/test/orm/demoorm/CustomInstantArrayType.java
@@ -0,0 +1,70 @@
+package ru.harper.test.orm.demoorm;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.usertype.UserType;
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.time.Instant;
+
+public class CustomInstantArrayType implements UserType {
+    @Override
+    public int[] sqlTypes() {
+        return new int[] {Types.ARRAY};
+    }
+
+    @Override
+            .
+    public Class returnedClass() {
+        return Instant[].class;
+    }
+
+    @Override
+    public boolean equals(Object o, Object o1) throws HibernateException {
+        return o == o1;
+    }
+
+    @Override
+    public int hashCode(Object o) throws HibernateException {
+        return o.hashCode();
+    }
+
+    @Override
+    public Object nullSafeGet(ResultSet resultSet, String[] strings, SharedSessionContractImplementor sharedSessionContractImplementor, Object o) throws HibernateException, SQLException {
+        return null;
+    }
+
+    @Override
+    public void nullSafeSet(PreparedStatement preparedStatement, Object o, int i, SharedSessionContractImplementor sharedSessionContractImplementor) throws HibernateException, SQLException {
+
+    }
+
+    @Override
+    public Object deepCopy(Object o) throws HibernateException {
+        return null;
+    }
+
+    @Override
+    public boolean isMutable() {
+        return false;
+    }
+
+    @Override
+    public Serializable disassemble(Object o) throws HibernateException {
+        return null;
+    }
+
+    @Override
+    public Object assemble(Serializable serializable, Object o) throws HibernateException {
+        return null;
+    }
+
+    @Override
+    public Object replace(Object o, Object o1, Object o2) throws HibernateException {
+        return null;
+    }
+}
